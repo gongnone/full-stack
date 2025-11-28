de72b61da41ce1ab7a2f379cc757b37c9c2f840c
gongnone <wjs@williamjshaw.ca>
2025-11-27 23:36:10 -0500

    feat: integrated hono app into data service worker and removed remote bindings flag
    Replaced placeholder Response with Hono app fetch handler in DataService WorkerEntrypoint. Removed --x-remote-bindings flag from dev script as remote bindings are now launched by default.

---
+++ apps/data-service/src/hono/app.ts
@@ -0,0 +1,12 @@
+import { Hono } from 'hono';
+
+
+export const App = new Hono<{ Bindings: Env }>();
+
+
+App.get('/:id', async (c) => {
+    
+    return c.json({
+        message: 'Hello World1!'
+    })
+})

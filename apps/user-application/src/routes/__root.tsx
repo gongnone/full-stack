0f60e6466f2fb672c57fac630fd28922f0d38008
gongnone <wjs@williamjshaw.ca>
2025-12-12 00:20:56 -0500

    feat: Conditionally lazy load TanStackRouterDevtools, rendering null in production and wrapping it with React.Suspense.

--- apps/user-application/src/routes/__root.tsx
+++ apps/user-application/src/routes/__root.tsx
@@ -1,9 +1,19 @@
 import { ThemeProvider } from "@/components/common/theme-provider";
 import type { QueryClient } from "@tanstack/react-query";
 import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+// import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import type { TRPCOptionsProxy } from "@trpc/tanstack-react-query";
 import type { AppRouter } from "@/worker/trpc/router";
+import React from "react";
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === "production"
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+      import("@tanstack/react-router-devtools").then((res) => ({
+        default: res.TanStackRouterDevtools,
+      }))
+    );
 
 export interface RouterAppContext {
   trpc: TRPCOptionsProxy<AppRouter>;
@@ -15,7 +25,9 @@ export const Route = createRootRouteWithContext<RouterAppContext>()({
     <>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <Outlet />
-        <TanStackRouterDevtools />
+        <React.Suspense>
+          <TanStackRouterDevtools />
+        </React.Suspense>
       </ThemeProvider>
     </>
   ),
